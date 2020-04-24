13e1b674ba10c27a7985ee89f5018a0ca1180e3c
Tomasz Więch <tewu.dev@gmail.com>
2020-04-24 13:30:01 +0200

    Bump to 1.0.0

--- src/pl/geek/tewu/celeste_extractor/AppInfo.java
+++ src/pl/geek/tewu/celeste_extractor/AppInfo.java
@@ -3,6 +3,6 @@ package pl.geek.tewu.celeste_extractor;
 public class AppInfo {
     public static final String NAME = "Celeste Extractor";
     public static final String SHORT_DESCRIPTION = "Converts .data files to .png losslessly";
-    public static final String VERSION = "1.0.0.rc2";
+    public static final String VERSION = "1.0.0";
     public static final String COMMAND = "java -jar CelesteExtractor.jar";
 }
